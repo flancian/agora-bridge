94fdbe64b9f0d370d7cd461980fe7a700ed0c4cb
Flancian <0@flancia.org>
2021-11-21 17:40:09 +0100

    Add output_dir to prod.

--- bots/twitter/run-dev.sh
+++ bots/twitter/run-dev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 . venv/bin/activate
-./agora-bot.py --config agora-bot.yaml --dry-run --max-age=60000 --output-dir=/home/flancian/agora/garden/an_agora $@ 
+OUTPUT=/home/agora/agora/garden/an_agora
+mkdir -p ${OUTPUT}
+./agora-bot.py --config agora-bot.yaml --dry-run --output-dir=${OUTPUT} $@ 
--- bots/twitter/run-prod.sh
+++ bots/twitter/run-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 . venv/bin/activate
-./agora-bot.py --config agora-bot.yaml $@
+OUTPUT=/home/agora/agora/garden/an_agora
+mkdir -p ${OUTPUT}
+./agora-bot.py --config agora-bot.yaml --output-dir=${OUTPUT} $@ 

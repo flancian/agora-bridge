55d21168f1c05b0f5c44345c3fd21aa1eb859e8d
Agora v0.5 <agora@anagora.org>
2022-09-18 19:45:41 +0200

    max_age is in minutes (sigh).

--- bots/twitter/run-prod.sh
+++ bots/twitter/run-prod.sh
@@ -2,4 +2,5 @@
 . venv/bin/activate
 OUTPUT=/home/agora/agora/stream/
 mkdir -p ${OUTPUT}
-./agora-bot.py --config agora-bot.yaml --output-dir=${OUTPUT} --max-age=604800 $@ 
+# 10080 = 7d in minutes
+./agora-bot.py --config agora-bot.yaml --output-dir=${OUTPUT} --max-age=10080 $@ 

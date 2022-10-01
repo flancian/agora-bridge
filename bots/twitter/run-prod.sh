c46d70182bd28e6a338da61d1ce9ea8a3c6c5269
Agora v0.5 <agora@anagora.org>
2022-10-01 22:09:49 +0200

    Friends list even loaded!

--- bots/twitter/run-prod.sh
+++ bots/twitter/run-prod.sh
@@ -3,6 +3,8 @@
 # . venv/bin/activate
 OUTPUT=/home/agora/agora/stream/
 mkdir -p ${OUTPUT}
+# This shouldn't be needed but it is when running as a systemd service for some reason.
+export PATH=$HOME/.local/bin:${PATH}
 # 10080 = 7d in minutes
 # 40320 = 4w in minutes
 poetry run ./agora-bot.py --config agora-bot.yaml --timeline --output-dir=${OUTPUT} --max-age=40320 $@ 

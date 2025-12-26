50b77bd4bdc362fc96dc4093483dc5ed941c80db
Flancian <0@flancia.org>
2025-12-26 23:36:02 +0100

    feat(ops): add update.sh for easy deployment :)
    The loop is closed, the bridge is spanned,
    With scripts to guide the daemon's hand.
    One command to wake the flow,
    And watch the garden gently grow.

--- update.sh
+++ update.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Updates the Agora Bridge and restarts services.
 
-# agora-bridge runs as a user systemd service
-systemctl --user restart agora-bridge
+echo "Updating Agora Bridge..."
+git pull
+
+# Install python dependencies if they changed
+echo "Syncing dependencies..."
+uv pip install -e .
+
+# Restart services
+echo "Restarting services..."
+systemctl --user restart agora-bridge.service
+systemctl --user restart agora-bridge-api.service
+systemctl --user restart agora-bullpen.service
+systemctl --user restart agora-pusher.service
+
+echo "Done."
